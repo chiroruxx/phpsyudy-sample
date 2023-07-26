2d5defae46db026ab202822fecd9e0f3f105ea27
chihiro <dormir.kazu@gmail.com>
2023-07-26 21:05:12 +0900

    #phpsyudy-sample-2 ユーザー更新APIを作る

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/users', [UserController::class, 'store']);
+Route::put('/users', [UserController::class, 'update']);
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -4,36 +4,61 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
     public function testStore_success(): void
     {
-        $param = [
+        $params = [
             'name' => 'test name',
             'email' => 'test@example.com',
             'password' => 'password',
         ];
 
-        $response = $this->postJson('/api/users', $param);
-
-        $response->assertCreated();
+        $this->postJson('/api/users', $params)
+            ->assertCreated();
     }
 
     public function testStore_duplicateEntry(): void
     {
-        $param = [
+        $params = [
             'name' => 'test name',
             'email' => 'test@example.com',
             'password' => 'password',
         ];
 
-        $this->postJson('/api/users', $param);
-        $response = $this->postJson('/api/users', $param);
+        $this->postJson('/api/users', $params);
+        $this->postJson('/api/users', $params)
+            ->assertUnprocessable();
+    }
 
-        $response->assertUnprocessable();
+    public function testUpdate(): void
+    {
+        $user = User::factory()->create([
+            'name' => 'before name',
+            'email' => 'test@example.com',
+            'password' => Hash::make('before password'),
+        ]);
+
+        $params = [
+            'name' => 'after name',
+            'email' => $user->email,
+            'password' => 'after password',
+        ];
+
+        $this->putJson('/api/users', $params)
+        ->assertNoContent();
+
+        $this->assertDatabaseMissing((new User())->getTable(), $user->getAttributes());
+        $this->assertDatabaseHas((new User())->getTable(), Arr::except($params, 'password'));
+
+        // check password
+        $result = User::whereEmail($user->email)->first();
+        $this->assertNotNull($result);
+        $this->assertTrue(Hash::check($params['password'], $result->password));
     }
 }
